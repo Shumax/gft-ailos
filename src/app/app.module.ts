d956f6923e93adb7e1cbc903eb4fba550b11eb79
Max Castro <maxcastro.mv@gmail.com>
2023-01-28 19:49:24 -0300

    feat: page cooperated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './shared/material-modules/material-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ const maskConfig: Partial<IConfig> = {
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    NgxMaskModule.forRoot(maskConfig)
+    NgxMaskModule.forRoot(maskConfig),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
